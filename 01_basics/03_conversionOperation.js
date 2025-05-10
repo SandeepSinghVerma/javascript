5925bb4fbe00d75b813b934009da4466fa8022fa
SandeepSinghVerma <149652963+SandeepSinghVerma@users.noreply.github.com>
2025-05-10 10:01:14 +0000

    conversionType

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,69 @@
+let score = 33
+// let score = "33abc"
+// let score = true
+// console.log(typeof score);
+// console.log(typeof(score));
+
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+
+let isloggedIn = 1
+// let booleanisloggedIn = Boolean(isloggedIn)
+// console.log(booleanisloggedIn);
+
+
+
+
+let a = 33
+let stringNumber = String(a)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// *********************** Operations ***********************
+
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1 = "hello"
+let str2 = " sandeep"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1"+ 2 + 2)
+// console.log(1 + 2+ "2")
+// console.log((3 +4) * 5 % 3);
+// console.log(+true);
+// console.log(+ "");
+
+// let num1 , num2, num3
+// num1 = num2 = num3 = 2+2
+
+
+let gameCounter = 100
+ ++gameCounter;
+// console.log(gameCounter);
+
+let x = 100
+ x++;
+// console.log(x);
