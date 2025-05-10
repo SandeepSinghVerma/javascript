e523a3f58dbb84c55485e3f642304bd724d92252
SandeepSinghVerma <149652963+SandeepSinghVerma@users.noreply.github.com>
2025-05-10 07:44:35 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 14533
+let accountEmail = "sandeepsinghverma34@gmail.com"
+var accountPassword = "12345"
+accountCity = "Bhopal"
+let accountstate;
+// accountId = 2 // Not allowed (TypeError: Assignment to constant variable.)
+accountEmail = "sandeepsinghverma216@gmail.com"
+accountPassword = "780098"
+accountCity = "Rewa"
+
+
+
+// console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountstate,accountCity])
