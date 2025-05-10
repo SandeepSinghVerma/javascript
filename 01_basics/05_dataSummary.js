c1cf2816aadf63c4af91a7ed8a5398d2a5124011
SandeepSinghVerma <149652963+SandeepSinghVerma@users.noreply.github.com>
2025-05-10 10:52:34 +0000

    data_summary

---
+++ 01_basics/05_dataSummary.js
@@ -0,0 +1,30 @@
+const score = 100
+const scoreValue = 100.3
+
+const isloggedIn = false
+const outsideTemp = null
+let userEmail;
+const id = Symbol('123')
+const anotherID = Symbol('123')
+console.log(id===anotherID);
+const bigNumber = 2334324243n
+
+const heros = ["Shaktiman", "Nagraj", "doga"]
+
+let myObj = {
+    name: "Sandeep",
+    age: 28
+}
+
+const myFunction = function() {
+    console.log("Hello World");}
+
+console.log(typeof score);
+console.log(typeof scoreValue);
+
+console.log(typeof isloggedIn);
+console.log(typeof userEmail);
+console.log(typeof id);
+console.log(typeof anotherID);
+console.log(typeof bigNumber);
+console.log(typeof heros);
