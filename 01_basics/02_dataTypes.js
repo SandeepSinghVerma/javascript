cb6875ab91b4fcbba1d0b1de915fb3fc237d432b
SandeepSinghVerma <149652963+SandeepSinghVerma@users.noreply.github.com>
2025-05-10 07:44:01 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict;" // treat all JS code as newer version
+
+//alert( 3 + 3 ) // we are using node.js , not browser.
+/*
+console.log ( 3 
+    + 3 // code readability should be high.
+)
+*/
+
+let name = 'sandeep'
+let age = 18
+let isloggedIn = false
+let state = null
+let city;
+
+console.log(typeof age)
+console.log(typeof isloggedIn)
+console.log(typeof city)
+console.log(typeof state)
